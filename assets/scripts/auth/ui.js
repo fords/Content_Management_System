7a5b4214d6740acac143963475f6da88c7602b75
clark-rob <clark.rob.g@gmail.com>
2018-12-16 19:46:26 -0500

    Authorization ui Changes
    - updated auth/ui to match class values on HTML
    - added example of Blogpost
    - created dropdown example
    - added possible form inputs to "Create Website modal"

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,62 +1,78 @@
+'use strict'
 const store = require('../store.js')
 
-// messages for user
 const signUpSuccess = data => {
-  $('#message').html('Signed up successfully')
-  $('#message').removeClass('failure')
-  $('#message').addClass('success')
-  $('#sign-up-form').addClass('hidden')
-  $('#change-password-form').addClass('hidden')
+  $('#up-message').show().text('What Games Do You Have?!')
+  $('#up-message').removeClass()
+  $('#up-message').addClass('success')
+  $('#up-message').fadeOut(5000)
+}
+
+const signUpFailure = () => { // removed error parameter
+  $('#up-message').show().text('Error on Sign Up')
+  $('#up-message').removeClass()
+  $('#up-message').addClass('failure')
+  // removed console.error
+  $('#up-message').fadeOut(9000)
 }
 
 const signInSuccess = data => {
   store.user = data.user
-  $('#message').html('Welcome Back!')
-  $('#message').addClass('success')
-  $('#message').removeClass('failure')
-  $('#sign-up-form').addClass('hidden')
-  $('#sign-in-form').addClass('hidden')
-  $('#change-password-form').removeClass('hidden')
-  $('#sign-out-button').removeClass('hidden')
-  $('#sign-out-button').show()
-  $('#change-password-form').show()
-  $('#sign-up-form').hide()
-  $('#sign-in-form').hide()
+  $('#up-message').removeClass()
+  $('#up-message').addClass('success')
+  $('#options-button').css('visibility', 'visible') // show options button
+  $('#sign-up-button').hide() // sign up is hidden
+  $('#authorizationModal').modal('hide') // closes modal after success
+}
+
+const signInFailure = () => { // removed error parameter
+  $('#up-message').show().text('Error on Sign In')
+  $('#up-message').removeClass()
+  $('#up-message').addClass('failure')
+  // removed console.error
+  $('#up-message').fadeOut(9000)
+}
+
+const changePasswordSuccess = data => {
+  $('#out-message').show().text('Your Password Has Been Changed!')
+  $('#out-message').removeClass()
+  $('#out-message').addClass('success')
+  $('#out-message').fadeOut(5000)
+}
+
+const changePasswordFailure = () => { // removed error parameter
+  $('#out-message').show().text('Error on password change')
+  $('#out-message').removeClass()
+  $('#out-message').addClass('failure')
+  // removed console.error
+  $('#out-message').fadeOut(9000)
 }
 
 const signOutSuccess = data => {
-  $('#message').html('Signed Out successfully')
   store.user = null
-  $('#message').removeClass('failure')
-  $('#message').addClass('success')
-  $('#sign-up-form').removeClass('hidden')
-  $('#sign-in-form').removeClass('hidden')
-  $('#change-password-form').addClass('hidden')
-  $('#sign-out-button').addClass('hidden')
-}
-const changePasswordSuccess = data => {
-  $('#message').html('You changed your password successfully.')
-  $('#message').addClass('success')
-  $('#message').removeClass()
-  $('#sign-up-form').addClass('hidden')
-  $('#sign-in-form').addClass('hidden')
-  $('#change-password-form').removeClass('hidden')
-  $('#sign-out-button').removeClass('hidden')
+  $('#out-message').removeClass()
+  $('#out-message').addClass('success')
+  // $('.current-player').hide()
+  $('#options-button').css('visibility', 'hidden') // sets option buton back to hidden
+  $('#sign-up-button').show() // sign up appears
+  $('#optionsModal').modal('hide') // closes modal after success
 }
 
-const failure = data => {
-  $('#message').html('Oops, something went wrong, please try again')
-  $('#message').css('color', 'red')
-  $('#message').removeClass('success')
-  $('#message').addClass('failure')
-  $('#sign-up-form').removeClass('hidden')
-  $('#sign-in-form').removeClass('hidden')
-  $('#change-password-form').removeClass('hidden')
+const signOutFailure = () => { // removed error parameter
+  $('#out-message').show().text('Error on Sign Out')
+  $('#out-message').removeClass()
+  $('#out-message').addClass('failure')
+  // removed console.error
+  $('#out-message').fadeOut(9000)
 }
+
 module.exports = {
-  changePasswordSuccess,
-  signOutSuccess,
-  signInSuccess,
   signUpSuccess,
-  failure
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
 }
