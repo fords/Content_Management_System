789b71757d344c2d8f16d34a57d8bc9ea76b01f6
efernandez573 <elsiefernandez@gmail.com>
2018-12-17 16:46:13 -0500

    peer programmed with Rob, still trying to link getallposts for front end, added content to index.html and app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,14 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events.js')
+const blogpostsEvents = require('./blog-posts/events.js')
+const websiteEvents = require('./websites/events.js')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#sign-out').on('submit', authEvents.onSignOut)
+  $('#change-password').on('submit', authEvents.onChangePassword)
+  $('#show-all-websites').on('click', blogpostsEvents.onGetAllPost)
 })
